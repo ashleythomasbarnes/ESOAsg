4058d7ae4ed24b6068580ffc784189550e1b66e0
Ashley Barnes <ashleybarnes.astro@gmail.com>
2024-12-09 19:46:11 +0100

    data info checks

--- ESOAsg/archive_science_portal.py
+++ ESOAsg/archive_science_portal.py
@@ -1,7 +1,8 @@
 from astropy import coordinates
 from astropy.coordinates import ICRS
+import webbrowser
 
-# from ESOAsg import msgs
+from ESOAsg import msgs
 from ESOAsg.ancillary import cleaning_lists
 from ESOAsg.core import asp_queries
 
@@ -79,3 +80,112 @@ def query_from_polygons(polygons=None, instruments=None, data_types=None, open_l
         asp_queries.run_query(url, show_link=show_link, open_link=open_link)
 
     return
+
+
+def open_url(url, show_link=True, open_link=True):
+    r"""Open some url in the browser
+
+    Args:
+        url (str): url to open
+        open_link (bool): open a link to page
+        show_link (bool): show the link on the terminal
+
+    Returns:
+        None
+
+    """
+    if show_link:
+        msgs.info('The link is:\n {}\n'.format(url))
+    if open_link:
+        webbrowser.open(url)
+    return
+
+def check_preview(dp_ids, show_link=False, open_link=True): 
+    """Check the preview of the data product
+        Note that this will download .pdf file to the browser download directory"""
+    msgs.info('This will download a .pdf file from the browser directory')
+
+    # Check if list
+    if isinstance(dp_ids, str):
+        dp_ids = [dp_ids]
+    dp_ids = cleaning_lists.from_element_to_list(dp_ids, element_type=str)
+    
+    # Loop over the dp_ids
+    for dp_id in dp_ids:
+        url = f'https://archive.eso.org/datalink/links?ID=ivo://eso.org/ID?{dp_id}&eso_download=preview'
+        open_url(url, show_link, open_link)
+
+
+def check_header(dp_ids, show_link=False, open_link=True): 
+    """Check the header of the data product"""
+    msgs.info('This will open a new tab with the header information in browser')
+
+    # Check if list
+    if isinstance(dp_ids, str):
+        dp_ids = [dp_ids]
+    dp_ids = cleaning_lists.from_element_to_list(dp_ids, element_type=str)
+
+    # Loop over the dp_ids
+    for dp_id in dp_ids:
+        url = f'https://archive.eso.org/hdr?DpId={dp_id}'
+        open_url(url, show_link, open_link)
+
+
+def check_details(dp_ids, show_link=False, open_link=True): 
+    """Check the details of the data product"""
+    msgs.info('This will open a new tab with the details in browser')
+
+    # Check if list
+    if isinstance(dp_ids, str):
+        dp_ids = [dp_ids]
+    dp_ids = cleaning_lists.from_element_to_list(dp_ids, element_type=str)
+
+    # Loop over the dp_ids
+    for dp_id in dp_ids:
+        url = f'https://archive.eso.org/dataset/{dp_id}'
+        open_url(url, show_link, open_link)
+
+
+def check_hips(dp_ids, show_link=False, open_link=True):
+    """Check the HIPS of the data product"""
+    msgs.info('This will open a new tab with the HIPS information in browser')
+
+    # Check if list
+    if isinstance(dp_ids, str):
+        dp_ids = [dp_ids]
+    dp_ids = cleaning_lists.from_element_to_list(dp_ids, element_type=str)
+
+    # Loop over the dp_ids
+    for dp_id in dp_ids:
+        url = f'https://archive.eso.org/previews/v1/files/{dp_id}/hips'
+        open_url(url, show_link, open_link)
+
+
+def check_release_info(dp_ids, show_link=False, open_link=True): 
+    """Check the release info of the data product"""
+    msgs.info('This will open a new tab with the release description infomation in browser')
+
+    # Check if list
+    if isinstance(dp_ids, str):
+        dp_ids = [dp_ids]
+    dp_ids = cleaning_lists.from_element_to_list(dp_ids, element_type=str)
+
+    # Loop over the dp_ids
+    for dp_id in dp_ids:
+        url = f'https://archive.eso.org/datalink/links?ID=ivo://eso.org/ID?{dp_id}&eso_download=data_documentation'
+        open_url(url, show_link, open_link)
+
+
+def check_metadata(dp_ids, show_link=False, open_link=True):
+    """Check the HTML metadata of the data product"""
+    msgs.info('This will open a new tab with the metadata information in browser')
+
+    # Check if list
+    if isinstance(dp_ids, str):
+        dp_ids = [dp_ids]
+    dp_ids = cleaning_lists.from_element_to_list(dp_ids, element_type=str)
+
+    # Loop over the dp_ids
+    for dp_id in dp_ids:
+        url = f'https://archive.eso.org/wdb/wdb/adp/phase3_main/query?dp_id={dp_id}'
+        open_url(url, show_link, open_link)
